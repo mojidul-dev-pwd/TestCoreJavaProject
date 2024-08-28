18150f2323845e89276be454512ed601395aeffe
Mojidul Islam <mojidul31@gmail.com>
2024-08-28 19:19:12 +0600

    user-input

---
+++ src/com/mt/test/UserInputExample.java
@@ -0,0 +1,16 @@
+package com.mt.test;
+
+import java.util.Scanner;
+
+public class UserInputExample {
+
+	public static void main(String[] args) {
+		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+	    System.out.println("Enter username");
+
+	    String userName = myObj.nextLine();  // Read user input
+	    System.out.println("Username is: " + userName);  // Output user input
+
+	}
+
+}
